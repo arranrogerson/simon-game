49396943275902b7e5cacb92d3a13f6ce1a4994c
arranrogerson <87150239+arranrogerson@users.noreply.github.com>
2024-10-16 01:05:22 -0700

    v1.1

--- script.js
+++ script.js
@@ -1,15 +1,81 @@
+// VARIABLES
 // The random button pattern created by the computer that the player must follow
 var computerChoices = [];
 // The button pattern chosen by players
 var playerChoices = [];
-
 // Four button colors
 var buttonColors = ["red", "blue", "green", "yellow"];
+// Level of the game
+var level = 0;
+// Has the game started switch
+var started = false;
 
-// Game mechanics
-function game() {
-	// Change the heading
-	$("h1").text("Follow the pattern");
+// GAME MECHANICS
+// If any key is pressed, start the game, computer chooses a square
+$(document).keypress(function() {
+	if (!started) {
+		// Change the heading
+		$("#main-heading").text(level);
+		nextSequence();
+		started = true;
+	}
+});
+
+// When it's the player's turn, and the player chooses a square...
+$(".square").on("click", function() {
+
+	// Get the color of the chosen square
+	playerChoice = $(this).attr("id");
+
+	// Record the choice to the playerChoicesArray, along with all their other choices for this level
+	playerChoices.push(playerChoice);
+
+	// Then animate the square
+	animateSquare(playerChoice);
+
+	// Play the sound
+	playSound(playerChoice);
+
+	// And see if the player got it right, if they didn't then game over
+	checkAnswer(playerChoices.length - 1); // This will check the most recent selection
+})
+
+
+// FUNCTIONS
+// Check to see if the player got their most recent choice right
+function checkAnswer(level) {
+	// If the player's choice matches computer's choice...
+	if (playerChoices[level] === computerChoices[level]) {
+		 
+		// and the player has finished their entire sequence
+		if (playerChoices.length === computerChoices.length) {
+
+			// After a one second pause, start the next level
+			setTimeout(function () {
+				nextSequence();
+			}, 1000);
+		}
+
+		// if the player hasn't finished their sequence, let them keep choosing (do nothing)
+
+	// ...otherwise, if the player's choice doesn't match the computer's choice, GAME OVER	
+	} else {
+		$("#main-heading").text("GAME OVER");
+		var finalScore = level + 1;
+		$("#final-score").text("You made it to level " + finalScore);
+		$("html").css("background-color", "red");
+		var wrong = new Audio("./sounds/wrong.mp3");
+		wrong.play();
+	}
+}
+
+// Start the next level
+function nextSequence() {
+	// reset the player's sequence
+	playerChoices = [];
+	// increase the level and reflect that in the heading
+	level++;
+	$("#main-heading").text(level);
 
 	// computer chooses a random number between 0 and 3
 	var randomNumber = Math.floor(Math.random()*4);
@@ -20,50 +86,21 @@ function game() {
 	// save the chosen button to the gamePattern array
 	computerChoices.push(randomChosenColor);
 
-	// then play the current button pattern
-	for (var i = 0; i < computerChoices.length; i++) {
-		// animate the chosen button...
-		$("#" + computerChoices[i]).fadeIn(100).fadeOut(100).fadeIn(100);
+	// Animate the square
+	animateSquare(randomChosenColor);
 
-		// ...and play the button's associated sound
-		var audio = new Audio("sounds/" + computerChoices[i] + ".mp3");
-		audio.play();
-
-		// pause for a moment
-		// setTimeout()
-	}
+	// Play the sound
+	playSound(randomChosenColor);
 
 }
 
-// If any key is pressed, start the game, computer chooses a square
-$(document).keypress(game);
+// Animate the chosen button
+function animateSquare(choice) {
+	$("#" + choice).fadeIn(100).fadeOut(100).fadeIn(100);
+}
 
-// When it's the player's turn, the player chooses a square...
-$(".square").on("click", function() {
-
-	// Get the color of the button
-	playerChoice = $(this).attr("id");
-
-	// Record the choice to the playerChoicesArray
-	playerChoices.push(playerChoice);
-
-	// If playerChoices matches computerChoices, continue the game
-	if (playerChoices == computerChoices) {
-		// animate the chosen button...
-		$("#" + playerChoice[i]).fadeIn(100).fadeOut(100).fadeIn(100);
-
-		// ...and play the button's associated sound
-		var audio = new Audio("sounds/" + playerChoice[i] + ".mp3");
-		audio.play();
-
-		// Then continue the game
-		game();
-
-	// ...otherwise GAME OVER	
-	} else {
-		$("h1").text("GAME OVER");
-		$("html").css("background-color", "red");
-		var wrong = new Audio("./sounds/wrong.mp3");
-		wrong.play();
-	}
-})
+// Play the button's associated sound
+function playSound(choice) {
+	var audio = new Audio("sounds/" + choice + ".mp3");
+	audio.play();
+}
